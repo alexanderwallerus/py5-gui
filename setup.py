e4aa6de530aa46ed206990c329c235425bc3ed17
alexanderwallerus <alex.wallerus@gmail.com>
2023-12-22 14:33:26 +0100

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                      version='0.1',
                      py_modules=['py5gui', 'utils.plot'],
                      packages=setuptools.find_packages(),
-                     package_data={'': ['fonts/roboto/Roboto-Regular.ttf', 'fonts/roboto/LICENSE.txt']},
+                     package_data={'fonts/roboto': ['*.ttf', '*.txt']}, # all .tff and .txt files
                      include_package_data=True,
                      install_requires=['pynput',]
                      )
